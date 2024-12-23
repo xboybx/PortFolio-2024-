84e3da8ed9c623538edabc765fdadbb7319f3987
Jaswanth <jeswanth012@gmail.com>
2024-12-23 16:56:45 +0530

    Update Vite configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/PortFolio-2024-/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx']
